9c3cf150e0825eac6ea4413bfc3e8f354a526d42
Henrique Lino <57471416+henriquelino@users.noreply.github.com>
2021-09-18 00:56:34 -0300

    update verify endpoint
    using URL params seens to return webpassword error, new method using post seens to solve this

--- Constants/ApiPoints.py
+++ Constants/ApiPoints.py
@@ -1,7 +1,7 @@
 
 VERSION = "https://static.drips.pw/rotmg/production/current/version.txt"
 EQUIP = "https://static.drips.pw/rotmg/production/current/xml/Equip.xml"
-VERIFY = "https://www.realmofthemadgod.com/account/verify?guid={}&password={}&clientToken={}&game_net=Unity&play_platform=Unity&game_net_user_id="
+VERIFY = "https://www.realmofthemadgod.com/account/verify"
 VERIFYTOKEN = "https://www.realmofthemadgod.com/account/verifyAccessTokenClient?clientToken={}&accessToken={}&game_net=Unity&play_platform=Unity&game_net_user_id"
 CHAR = "https://www.realmofthemadgod.com/char/list?do_login=true&accessToken={}&game_net=Unity&play_platform=Unity&game_net_user_id="
 
