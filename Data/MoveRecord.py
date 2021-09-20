280b8b909c92b01a4845f81773532007d76ede9f
him <im@him.is>
2021-09-20 19:13:07 +0100

    fix MoveRecord.write variables
    + update types to float

--- Data/MoveRecord.py
+++ Data/MoveRecord.py
@@ -1,19 +1,19 @@
 
 class MoveRecord:
-    def __init__(self, time=0, x=0, y=0):
-        self.time = time
-        self.x = x
-        self.y = y
+    def __init__(self, time=0, x=0.0, y=0.0):
+        self.time: int = time
+        self.x: float = x
+        self.y: float = y
 
-    def read(self, reader):
+    def read(self, reader) -> None:
         self.time = reader.readInt32()
         self.x = reader.readFloat()
         self.y = reader.readFloat()
 
-    def write(self, writer):
-        writer.writeInt32(time)
-        writer.writeFloat(y)
-        writer.writeFloat(y)
+    def write(self, writer) -> None:
+        writer.writeInt32(self.time)
+        writer.writeFloat(self.y)
+        writer.writeFloat(self.y)
 
-    def clone(self):
+    def clone(self) -> object:
         return MoveRecord(self.time, self.x, self.y)
