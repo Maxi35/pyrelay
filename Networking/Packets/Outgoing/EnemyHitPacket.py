e34a61e5546daaa405745d505044d3149c772ee1
him <im@him.is>
2021-10-05 04:58:51 +0100

    update EnemyHit

--- Networking/Packets/Outgoing/EnemyHitPacket.py
+++ Networking/Packets/Outgoing/EnemyHitPacket.py
@@ -1,19 +1,27 @@
+
 class EnemyHitPacket:
+    """ Sent when a projectile from you or a close-by player hits an enemy """
     def __init__(self):
         self.type = "ENEMYHIT"
-        self.time = 0
-        self.bulletId = 0
-        self.targetId = 0
-        self.kill = False
+        self.time: int = 0  # int32
+        self.bulletId: int = 0  # int16
+        self.targetId: int = 0  # int32
+        self.ownerId: int = 0  # int32
+        self.killed: bool = False  # bool
+        self.projectileOwnerId: int = 0  # int32
 
     def write(self, writer):
         writer.writeInt32(self.time)
         writer.writeShort(self.bulletId)
         writer.writeInt32(self.targetId)
-        writer.writeBool(self.kill)
+        writer.writeInt32(self.ownerId)
+        writer.writeBool(self.killed)
+        writer.writeInt32(self.projectileOwnerId)
 
     def read(self, reader):
         self.time = reader.readInt32()
         self.bulletId = reader.readShort()
         self.targetId = reader.readInt32()
-        self.kill = reader.readBool()
+        self.ownerId = reader.readInt32()
+        self.killed = reader.readBool()
+        self.projectileOwnerId = reader.readInt32()
