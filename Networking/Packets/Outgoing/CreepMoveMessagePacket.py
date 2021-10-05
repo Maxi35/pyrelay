2302a4dfac8b931046098f9d4548c1eb8aefebc5
him <im@him.is>
2021-10-05 06:00:46 +0100

    fix CreepMoveMessage

--- Networking/Packets/Outgoing/CreepMoveMessagePacket.py
+++ Networking/Packets/Outgoing/CreepMoveMessagePacket.py
@@ -1,10 +1,12 @@
+from Data.WorldPosData import WorldPosData
+
 
 class CreepMoveMessagePacket:
     """ Sent when a summoner creep is moved """
     def __init__(self):
-        self.type = "PONG"
+        self.type = "CREEPMOVEMESSAGE"
         self.objectId: int = 0  # int32
-        self.position: object = WorldPosData()  # WorldPosData
+        self.position: WorldPosData = WorldPosData()  # WorldPosData
         self.hold: bool = False  # bool
 
     def write(self, writer):
@@ -14,5 +16,5 @@ class CreepMoveMessagePacket:
 
     def read(self, reader):
         self.objectId = reader.readInt32()
-        self.position = read(reader)
+        self.position.read(reader)
         self.hold = reader.readBool()
